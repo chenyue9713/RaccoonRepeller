d89348c8f2d3490fb28a4844cccc1cb4390221f1
Yue Chen <cy422311@gmail.com>
2021-12-06 03:33:16 +0000

    Add py script and model file for model init and prediction

--- software/src/main.py
+++ software/src/main.py
@@ -1,42 +1,16 @@
-import argparse
-import os
-
-from detection.run_tf_detector_batch import load_and_run_detector_batch
+from edgetpumodel import EdgeTPUModel
 
 # model path
-model_file = "/home/yue/RaccoonRepeller/software/model/md_v4.1.0.pb"
-image_file = "/home/yue/RaccoonRepeller/software/images/Raccoon_1.jpg"
+model_file = "/home/mendel/RaccoonRepeller/software/model/best1-fp16.tflite"
+image_file = "/home/mendel/RaccoonRepeller/software/images/Raccoon_3.jpg"
+names = "/home/mendel/RaccoonRepeller/software/images/data.yaml"
 
-def process_image(file_name):
-  # This runs the detector itself and tosses the output in an
-  # output.json file.
-  return load_and_run_detector_batch(
-    model_file=model_file,
-    image_file_names=[file_name],
-    checkpoint_path='./output.json',
-    confidence_threshold=0.85
-  )
-
-def is_animal_image(ml_result):
-  for detection in ml_result['detections']:
-    # A detection of '1' is an animal ('2' is a human)
-    if detection['category'] == '1':
-      return True
-  return False
 
 def main():
 
-    # This is the new stuff that processes the image from
-    # the camera. If the image does not contain an animal,
-    # delete the image.
+    model = EdgeTPUModel(model_file, names)
 
-    ml_result = process_image(image_file)[0]
-    print(ml_result)
-    if is_animal_image(ml_result):
-        print('Animal detected!')
-    else:
-        print('No animal detected')
-    # os.remove(image_name)
+    model.predict(image_file)
 
 
 main()
